fc4b983af769f91a2451b39d236212b8975fdaaa
Stas <stas@MacBook-Air-Stas.local>
2024-04-30 17:55:51 +0300

    add count button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState } from 'react'
 import {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
@@ -7,18 +8,27 @@ import ProductList from "./components/ProductList/ProductList";
 import Form from "./components/Form/Form";
 import twaLogo from './assets/balls.png'
 function App() {
-    const {onToggleButton, tg} = useTelegram();
 
+    const {onToggleButton, tg} = useTelegram();
+    const [count, setCount] = useState(0)
     useEffect(() => {
         tg.ready();
     }, [])
 
     return (
-        <div className="App">
-            <a href="https://ton.org/dev" target="_blank">
-                <img src={twaLogo} className="logo" alt="TWA logo"/>
-            </a>
-        </div>
+        <>
+            <div className="App">
+                <a href="https://ton.org/dev" target="_blank">
+                 <img src={twaLogo} className="logo" alt="TWA logo"/>
+                </a>
+            </div>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    <img src={twaLogo} className="logo" alt="TWA logo"/>
+                    count is {count}
+                </button>
+            </div>
+        </>
     );
 }
 
